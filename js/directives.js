90bfa28798268530786c47fffc3ba1107beefc2b
Guille Paz <guille87paz@gmail.com>
2015-02-01 21:28:35 -0300

    Add form validations.

--- js/directives.js
+++ js/directives.js
@@ -1,31 +1,41 @@
-angular.module('mango', []).directive('mangoForm', ['$window',
-function($window) {
+angular.module('mango', [])
+  .directive('mangoForm', ['$window', function($window) {
 
-  var directive = { restrict: 'A' };
-  directive.link = function(scope, element, attributes) {
-    var form = angular.element(element);
-    form.bind('submit', function() {
+    var directive = { restrict: 'A' };
+
+    directive.link = function(scope, element, attributes) {
+      var form = angular.element(element);
       var button = form.find('button');
-      button.prop('disabled', true);
 
-      var data = {
-        'holdername': scope.holdername,
-        'number': scope.number,
-        'type': scope.type,
-        'exp_month': scope.exp_month,
-        'exp_year': scope.exp_year,
-        'ccv': scope.ccv
-      };
+      form.bind('submit', function() {
 
-      $window.Mango.token.create(data, function() {
-        button.prop('disabled', false);
-        var args = arguments;
-        scope.$apply(function() {
-          scope[attributes.mangoForm].apply(scope, args);
+        if (!scope.mango.$valid) {
+          return;
+        }
+
+        button.prop('disabled', true);
+
+        var data = {
+          'holdername': scope.holdername,
+          'number': scope.number,
+          'type': scope.type,
+          'exp_month': scope.expiry.getMonth() + 1,
+          'exp_year': scope.expiry.getFullYear(),
+          'ccv': scope.ccv
+        };
+
+        $window.Mango.token.create(data, function() {
+          button.prop('disabled', false);
+          var args = arguments;
+          scope.$apply(function() {
+            scope[attributes.mangoForm].apply(scope, args);
+          });
         });
-      });
-    });
-  };
-  return directive;
 
-}]);
+      });
+
+    };
+
+    return directive;
+
+  }]);
